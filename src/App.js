871331c80add5cea053f0586d6690d002eaa4428
Erin Hales <mcbridee093@gmail.com>
2018-08-15 16:32:03 -0600

    updating with master

--- src/App.js
+++ src/App.js
@@ -4,24 +4,14 @@ import './styles/main.css';
 // import Login from './components/Login/Login';
 // import Pens from './components/Pens/Pens';
 import CodeEditor from './components/CodeEditor/CodeEditor';
-<<<<<<< HEAD
-=======
 import NavBar from './components/NavBar/NavBar'
->>>>>>> master
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        {/* <Pens /> */}
-        <CodeEditor />
-        {/* <Login /> */}
-        {/* <CodeEditor /> */}
-=======
         <NavBar/>
         <CodeEditor />
->>>>>>> master
       </div>
     );
   }
