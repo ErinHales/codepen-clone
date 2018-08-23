efc10680822004980d0cf02b19898b1520ec754c
Michael Fearn <fearn.michael@gmail.com>
2018-08-23 11:32:13 -0600

    implemeneted pen settings on editor page

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -6,6 +6,7 @@ import "./Theme.css";
 import axios from 'axios';
 import SignUp from '../SignUp/SignUp'
 import Login from '../Login/Login';
+import PenSettings from '../PenSettings/PenSettings';
 require('codemirror/lib/codemirror.css');
 
 
@@ -14,6 +15,7 @@ export default class CodeEditor extends Component {
         super();
 
         this.state = {
+            showSettings:true,
             showPopUp: false,
             showSignUp: false,
             isLoggedIn: false,
@@ -23,8 +25,45 @@ export default class CodeEditor extends Component {
             html: '',
             js: '',
             userid: null,
-            theme: "abcdef"
+            theme: "abcdef",
+
+            behaviorSettings: {
+                tabSize: 0,
+                autoSave: false,
+                autoUpdate: true,
+            },
+            jsSettings: {
+                jsCdnList: []
+            },
+            cssSettings: {
+                cssCdnList: []
+            },
+            htmlSettings: {
+                htmlClassTag: '',
+                head: '',
+            },
+            pageSelection : {
+                htmlPage: true,
+                cssPage: false,
+                jsPage: false,
+                behaviorPage: false
+            }
         }
+
+        this.settingsPageSelectionHandler = this.settingsPageSelectionHandler.bind(this)
+
+        this.jsCdnSelectHandler = this.jsCdnSelectHandler.bind(this)
+        this.removeJsCdn = this.removeJsCdn.bind(this)
+        
+        this.cssCdnSelectHandler = this.cssCdnSelectHandler.bind(this)
+        this.removeCssCdn = this.removeCssCdn.bind(this)
+
+        this.headStuffHandler = this.headStuffHandler.bind(this)
+        this.classTagHandler = this.classTagHandler.bind(this)
+
+        this.autoSaveHandler = this.autoSaveHandler.bind(this)
+        this.autoUpdateHandler = this.autoUpdateHandler.bind(this)
+        this.tabSizeHandler = this.tabSizeHandler.bind(this)
     }
 
     componentWillMount() {
@@ -64,7 +103,7 @@ export default class CodeEditor extends Component {
             theme: e.target.value
         })
     }
-
+    ////// DATA FORMATING ///////
     penData = () => {
         const {name, html, css, js} = this.state
         return {
@@ -75,20 +114,15 @@ export default class CodeEditor extends Component {
             js,
             scripts: {
                 html: {
-                    html_tag_class: '',
-                    head_tag: ''
+                    html_tag_class: this.state.htmlSettings.htmlClassTag,
+                    head_tag: this.state.htmlSettings.head
                 },
-                css: [],
-                js: []
+                css: this.state.cssSettings.cssCdnList,
+                js: this.state.jsSettings.jsCdnList
             }
         }
     }
-
-    updatePen = () => {
-        
-        console.log("This is not set up yet");
-    }
-
+    /////// SERVER INTERACTION //////////////
     savePen = () => {
         if(!this.state.isLoggedIn) {
             this.setState({showPopUp: true})
@@ -106,6 +140,14 @@ export default class CodeEditor extends Component {
         }
     }
 
+    deletePen = () => {
+        axios.delete(`/api/pen/${this.props.match.params.id}`)
+            .then()
+            .catch(console.error)
+    }
+
+
+    ////////// UPDATING WORKING IFRAME /////////////
     updateCSS = (newCode) => {
         this.setState({
             css: newCode
@@ -123,6 +165,127 @@ export default class CodeEditor extends Component {
             html: newCode
         })
     }
+
+    //////////// SETTINGS MENU //////////////////
+    //JS page Handlers
+    jsCdnSelectHandler(data) {
+        console.log(data)
+        this.setState({
+            jsSettings: {
+                jsCdnList: [...this.state.jsSettings.jsCdnList, data.latest]
+                }
+        })
+    }
+    removeJsCdn(value) {
+        console.log('fired')
+        this.setState({
+            jsSettings: {
+                jsCdnList: this.state.jsSettings.jsCdnList.filter(e => !e.startsWith(value))
+            }
+        })
+    }
+
+    //CSS page Handlers
+    cssCdnSelectHandler(data) {
+        console.log(data)
+        this.setState({
+            cssSettings: {
+                cssCdnList: [...this.state.cssSettings.cssCdnList, data.latest]
+                }
+        })
+    }
+    removeCssCdn(value) {
+        this.setState({
+            cssSettings: {
+                cssCdnList: this.state.cssSettings.cssCdnList.filter(e => !e.startsWith(value))
+            }
+        })
+    }
+
+    //HTML Page Handlers
+    classTagHandler(value) {
+        console.log('tag handler fired')
+        this.setState({
+            htmlSettings: {
+                htmlClassTag: value
+            }
+        })
+    }
+    headStuffHandler(value) {
+        this.setState({
+            htmlSettings: {
+                head: value
+            }
+        })
+    }
+
+    // Behavior Page Handlers
+    autoSaveHandler(value) {
+        console.log(2222,value)
+        this.setState({
+            behaviorSettings: Object.assign({}, this.state.behaviorSettings, {autoSave:value})
+        })
+    }
+    autoUpdateHandler(value) {
+        this.setState({
+            behaviorSettings: Object.assign({}, this.state.behaviorSettings, {autoUpdate:value})
+        })
+        
+    }
+    tabSizeHandler(value) {
+        this.setState({
+            behaviorSettings : Object.assign({}, this.state.behaviorSettings, {tabSize:value})
+        })
+    }
+
+    // settings page selection
+    settingsPageSelectionHandler(pageName) {
+        const pageChanger = {
+            html: () => this.setState({
+                pageSelection: Object.assign({}, this.state.pageSelection, {
+                    htmlPage: true,
+                    cssPage: false,
+                    jsPage: false,
+                    behaviorPage: false
+                }) 
+            }),
+            css: () => this.setState({
+                pageSelection: Object.assign({}, this.state.pageSelection, {
+                    htmlPage: false,
+                    cssPage: true,
+                    jsPage: false,
+                    behaviorPage: false
+                }) 
+            }),
+            js: () => this.setState({
+                pageSelection: Object.assign({}, this.state.pageSelection, {
+                    htmlPage: false,
+                    cssPage: false,
+                    jsPage: true,
+                    behaviorPage: false
+                }) 
+            }),
+            behavior: () => this.setState({
+                pageSelection: Object.assign({}, this.state.pageSelection, {
+                    htmlPage: false,
+                    cssPage: false,
+                    jsPage: false,
+                    behaviorPage: true
+                }) 
+            }),
+        }
+        pageChanger[pageName]()
+        this.settingsPopUpHandler(true)
+    }
+
+    //// SETTINGS POPUP CONTROLLERS ////////
+    settingsPopUpHandler = (value) => {
+        this.setState({
+            showSettings: value
+        })
+    }
+
+    //////////// ACCOUNT VALIDATION //////////////
     closePopUp = (bool = false) => { 
         if(bool) {
             this.setState({
@@ -140,13 +303,39 @@ export default class CodeEditor extends Component {
     popUpSwitch = () => {
         this.setState({showSignUp: !this.state.showSignUp})
     }
-    deletePen = () => {
-        axios.delete(`/api/pen/${this.props.match.params.id}`)
-            .then()
-            .catch(console.error)
-    }
+    ///////////////////////////////
 
     render() {
+        
+        const settingsMenu = (
+            <div className="pen-settings-popup-container">
+                <PenSettings 
+                    settingsPopUpHandler={this.settingsPopUpHandler}
+                    htmlSettings={this.state.htmlSettings}
+                    classTagHandler={this.classTagHandler}
+                    headStuffHandler={this.headStuffHandler}
+
+                    cssSettings={this.state.cssSettings}
+                    cssCdnSelectHandler={this.cssCdnSelectHandler}
+                    removeCssCdn={this.removeCssCdn}
+
+                    jsSettings={this.state.jsSettings}
+                    jsCdnSelectHandler={this.jsCdnSelectHandler}
+                    removeJsCdn={this.removeJsCdn}
+
+                    behaviorSettings={this.state.behaviorSettings}
+                    autoSaveHandler={this.autoSaveHandler}
+                    autoUpdateHandler={this.autoUpdateHandler}
+                    tabSizeHandler={this.tabSizeHandler}
+                    
+                    pageSelection={this.state.pageSelection}
+                    pageHandler={this.settingsPageSelectionHandler}
+                />
+                <div onClick={() => this.settingsPopUpHandler(false)} className="pen-settings-popup-overlay"></div>
+            </div>
+        )
+
+
         const popUp = (
             <div className="signup-popup">
             <div className="signup-popup-form-container">
@@ -165,24 +354,31 @@ export default class CodeEditor extends Component {
         return (
             <div className="codeEditor">
             {this.state.showPopUp ? popUp : null}
+            {this.state.showSettings ? settingsMenu : null}
                 <div className="editorHead">
                 <section className="editorSection">
                     <div>
-                        <button><img className="settingsImg" src="http://www.clker.com/cliparts/5/t/n/f/d/T/white-gear-hi.png" alt="settings"/></button>
+                        <button
+                            onClick={() => this.settingsPageSelectionHandler('html')}
+                            ><img className="settingsImg" src="http://www.clker.com/cliparts/5/t/n/f/d/T/white-gear-hi.png" alt="settings"/></button>
                         <h3>HTML</h3>
                     </div>
                     <button><img className="settingsImg" src="http://i66.tinypic.com/2gufexh.jpg" alt="down arrow" /></button>
                 </section>
                 <section className="editorSection">
                     <div>
-                        <button><img className="settingsImg" src="http://www.clker.com/cliparts/5/t/n/f/d/T/white-gear-hi.png" alt="settings"/></button>
+                        <button
+                            onClick={() => this.settingsPageSelectionHandler('css')}
+                        ><img className="settingsImg" src="http://www.clker.com/cliparts/5/t/n/f/d/T/white-gear-hi.png" alt="settings"/></button>
                         <h3>CSS</h3>
                     </div>
                     <button><img className="settingsImg" src="http://i66.tinypic.com/2gufexh.jpg" alt="down arrow" /></button>
                 </section>
                 <section className="editorSection">
                     <div>
-                        <button><img className="settingsImg" src="http://www.clker.com/cliparts/5/t/n/f/d/T/white-gear-hi.png" alt="settings"/></button>
+                        <button
+                        onClick={() => this.settingsPageSelectionHandler('js')}
+                        ><img className="settingsImg" src="http://www.clker.com/cliparts/5/t/n/f/d/T/white-gear-hi.png" alt="settings"/></button>
                         <h3>JS</h3>
                     </div>
                     <button><img className="settingsImg" src="http://i66.tinypic.com/2gufexh.jpg" alt="down arrow" /></button>
