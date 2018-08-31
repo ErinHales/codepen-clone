f4a747779e2d56bbf5764895bd01edfad74034c5
Your Name <you@example.com>
2018-08-30 19:31:02 -0600

    changed the amount of time for the alert

--- src/components/Account/Showcase.js
+++ src/components/Account/Showcase.js
@@ -57,7 +57,7 @@ class Showcase extends Component {
             document.querySelector('.alert').style.display = 'block';
             setTimeout(() =>{
                 document.querySelector('.alert').style.display = 'none';
-            }, 3000)
+            }, 2000)
         }, 875);
     //    document.querySelector('.alert').style.display = 'block';
 }
